28b11735023e31f263dc680658f1d1db0483c468
Rachel Chen <rschen@ncsu.edu>
2023-05-01 16:05:15 -0400

    Add comments for chessGPTutor

--- chessGPTutor.py
+++ chessGPTutor.py
@@ -1,16 +1,14 @@
 import json, argparse, openai
 import chess.engine
 
+
+# Parse arguments for human player's side and opponent's level
 parser = argparse.ArgumentParser()
 parser.add_argument("--side", help="which side to play as; options: {white, black}", type=str)
 parser.add_argument("--level", help="level of difficulty of AI opponent; options {easy, medium, hard}", type=str)
 args = parser.parse_args()
 
-
-f = open("./config.json")
-data = json.load(f)
-f.close()
-
+# Exit if arguments are invalid
 if args.side not in ["white","black"]:
     exit('Invalid player. Please enter "white" or "black"')
 elif args.level not in ["easy","medium","hard"]:
@@ -20,32 +18,44 @@ elif args.level not in ["easy","medium","hard"]:
 # Start up game window
 import src.gameplay as gp
 
+# Parse config.json for OpenAI API key and Stockfish path
+f = open("./config.json")
+data = json.load(f)
+f.close()
+
+# Initialize players
 if args.side == "white":
+    # Human is the white player
     human_black = False
     p1 = gp.HumanPlayer(color = True)
 
     if args.level == "easy":
+        # Easy opponent - alpha-beta
         p2 = gp.ABPlayer(color = False, algo = "AB_fail_soft", depth=3)
     elif args.level == "medium":
+        # Medium opponent - Stockfish
         p2 = gp.StockfishPlayer(path = data['STOCKFISH_PATH'], color = False, depth = 4)
     else:
+        # Hard opponent - Stockfish
         p2 = gp.StockfishPlayer(path = data['STOCKFISH_PATH'], color = False)
-
 else:
+    # Human is the black player
     human_black = True
     p2 = gp.HumanPlayer(color = False)
 
     if args.level == "easy":
+        # Easy opponent - alpha-beta
         p1 = gp.ABPlayer(color = True, algo = "AB_fail_soft", depth=3)
     elif args.level == "medium":
+        # Medium opponent - Stockfish
         p1 = gp.StockfishPlayer(path = data['STOCKFISH_PATH'], color = True, depth = 4)
     else:
+        # Hard opponent - Stockfish
         p1 = gp.StockfishPlayer(path = data['STOCKFISH_PATH'], color = True)
 
-
+# Initialize ChatGPT instance and prime with introduction
 openai.api_key = data['OPENAI_API_KEY']
 model_engine = "gpt-3.5-turbo"
-
 intro_message = f"You are a chess tutor for a beginner player playing as {args.side}."
 
 response = openai.ChatCompletion.create(
@@ -60,4 +70,5 @@ response = openai.ChatCompletion.create(
 message = response.choices[0]['message']
 print(f"Chess tutor for {args.side} player is ready. Have fun!")
 
+# Play game
 gp.play_game(p1, p2, human_black)
